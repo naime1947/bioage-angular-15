bfcc302ae8073a38bbf649d3aebe0ba3249f33c6
Naime Ahmed <naime.ahmed@metadesignsolutions.co.uk>
2023-04-14 23:38:24 +0600

    disclaimer page done both

--- src/app/modules/disclaimers/disclaimers.module.ts
+++ src/app/modules/disclaimers/disclaimers.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DisclaimersComponent } from './disclaimers.component';
+import { DisclaimersRoutingModule } from './disclaimers-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DisclaimersComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    DisclaimersRoutingModule
   ]
 })
 export class DisclaimersModule { }
