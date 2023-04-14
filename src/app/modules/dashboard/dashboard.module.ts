45c8207805fade1afdaf252b92d8df4eeb252810
Naime Ahmed <naime.ahmed@metadesignsolutions.co.uk>
2023-04-14 17:07:56 +0600

    dashboard component created and banner started

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { SharedModule } from '@shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    DashboardRoutingModule,
+    SharedModule
   ]
 })
 export class DashboardModule { }
