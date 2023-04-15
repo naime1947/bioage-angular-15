1ae17e8e7950dc84ab15a52e8c94f8accda7dea0
Naime Ahmed <naime.ahmed@metadesignsolutions.co.uk>
2023-04-15 14:55:20 +0600

    action tab banner done

--- src/app/modules/actions/actions.module.ts
+++ src/app/modules/actions/actions.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActionsComponent } from './actions.component';
+import { ActionsRoutingModule } from './actions-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ActionsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ActionsRoutingModule
   ]
 })
 export class ActionsModule { }
