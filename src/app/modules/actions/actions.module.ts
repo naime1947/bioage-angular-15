f64936039b0e8afe73bab48af5e8ed357309117d
Naime Ahmed <naime.ahmed@metadesignsolutions.co.uk>
2023-04-16 16:10:50 +0600

    mobile version upto section two done

--- src/app/modules/actions/actions.module.ts
+++ src/app/modules/actions/actions.module.ts
@@ -3,17 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ActionsComponent } from './actions.component';
 import { ActionsRoutingModule } from './actions-routing.module';
 import { SharedModule } from '@shared/shared.module';
-
-
+import { ThreeBoxComponent } from './component/three-box/three-box.component';
+import { SeeReferenceComponent } from './component/see-reference/see-reference.component';
 
 @NgModule({
-  declarations: [
-    ActionsComponent
-  ],
-  imports: [
-    CommonModule,
-    ActionsRoutingModule,
-    SharedModule
-  ]
+  declarations: [ActionsComponent, ThreeBoxComponent, SeeReferenceComponent],
+  imports: [CommonModule, ActionsRoutingModule, SharedModule],
 })
-export class ActionsModule { }
+export class ActionsModule {}
