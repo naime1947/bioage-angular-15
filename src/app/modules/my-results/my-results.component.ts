4454acadd797fdbe15183264fb77910479e5adde
Naime Ahmed <naime.ahmed@metadesignsolutions.co.uk>
2023-04-20 17:16:17 +0600

    my results header

---
+++ src/app/modules/my-results/my-results.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-results',
+  templateUrl: './my-results.component.html',
+  styleUrls: ['./my-results.component.scss']
+})
+export class MyResultsComponent {
+
+}
--- src/app/modules/my-results/my-results.module.ts
+++ src/app/modules/my-results/my-results.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MyResultsComponent } from './my-results.component';
+import { MyResultsRoutingModule } from './my-results-routing.module';
+import { SharedModule } from '@shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MyResultsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MyResultsRoutingModule,
+    SharedModule
   ]
 })
 export class MyResultsModule { }
