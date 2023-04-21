c236b491251600ad8104e32ddcbf6bdb2190dc10
Naime Ahmed <naime.ahmed@metadesignsolutions.co.uk>
2023-04-21 15:02:10 +0600

    science component and routing created

---
+++ src/app/modules/the-science/the-science.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-the-science',
+  templateUrl: './the-science.component.html',
+  styleUrls: ['./the-science.component.scss']
+})
+export class TheScienceComponent {
+
+}
