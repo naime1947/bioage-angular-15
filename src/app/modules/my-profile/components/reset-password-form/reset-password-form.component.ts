cace3bb53eb502f1e89d5f0189a2f66185ed81d0
Naime Ahmed <naime.ahmed@metadesignsolutions.co.uk>
2023-04-25 12:19:43 +0600

    show hide password done

--- src/app/modules/my-profile/components/reset-password-form/reset-password-form.component.ts
+++ src/app/modules/my-profile/components/reset-password-form/reset-password-form.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password-form',
   templateUrl: './reset-password-form.component.html',
-  styleUrls: ['./reset-password-form.component.scss']
+  styleUrls: ['./reset-password-form.component.scss'],
 })
-export class ResetPasswordFormComponent {
+export class ResetPasswordFormComponent implements OnInit {
+  passwordForm!: FormGroup;
+  showNewPassword = false;
+  showConfirmPassword = false;
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.passwordForm = this.fb.group({
+      newPassword: [
+        '',
+        [
+          Validators.pattern(
+            '^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$'
+          ),
+        ],
+      ],
+      confirmPassword: [
+        '',
+        [
+          Validators.pattern(
+            '^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$'
+          ),
+        ],
+      ],
+    });
+  }
+
+  get f() {
+    return this.passwordForm.controls;
+  }
 
 }
