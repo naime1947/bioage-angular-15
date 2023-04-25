14107d591ed250b029b3552cd46024393b749426
Naime Ahmed <naime.ahmed@metadesignsolutions.co.uk>
2023-04-25 18:05:12 +0600

    font fix

--- src/app/modules/my-profile/my-profile-routing.module.ts
+++ src/app/modules/my-profile/my-profile-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { MyProfileComponent } from "./my-profile.component";
 import { CloseAccountComponent } from "./pages/close-account/close-account.component";
+import { ManageAccountComponent } from "./pages/manage-account/manage-account.component";
+import { ResetPasswordComponent } from "./pages/reset-password/reset-password.component";
 
 const routes:Routes = [
   {
     path: '',
     component: MyProfileComponent
+  },
+  {
+    path: 'manage-account',
+    component: ManageAccountComponent
+  },
+  {
+    path: 'reset-password',
+    component: ResetPasswordComponent
   },
   {
     path: 'close-account',
