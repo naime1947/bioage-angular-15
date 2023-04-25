d5ace3a6a5ae87929d1f0543250ce4c61d2ff15e
Naime Ahmed <naime.ahmed@metadesignsolutions.co.uk>
2023-04-25 19:55:19 +0600

    edit personal detail mobile version done

--- src/app/modules/my-profile/pages/manage-account/manage-account.component.ts
+++ src/app/modules/my-profile/pages/manage-account/manage-account.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserModel } from '@modules/auth/models/user.model';
+import { AuthService } from '@modules/auth/services/auth.service';
+import { PersonalDetailModel } from '@shared/models/personal-detail.model';
 
 @Component({
   selector: 'app-manage-account',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./manage-account.component.scss']
 })
 export class ManageAccountComponent {
+  user: UserModel | null;
 
+  personalDetail: PersonalDetailModel;
+
+  constructor(private authService: AuthService) {
+    this.user = authService.getUser();
+    this.personalDetail = {
+      displayName: this.user?.displayName!,
+      email: this.user?.email!,
+      phone: this.user?.phone!,
+      country: this.user?.country!
+    }
+  }
 }
